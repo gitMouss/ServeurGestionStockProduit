ae3880f84ff1f83489c0db1ef50bb356401f10b5
gitMousss <touremouss@hotmail.fr>
2020-01-21 12:24:53 +0100

    first commit

---
+++ src/test/java/org/gestion/produit/ServeurGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestion.produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
